d993fcd69c9a771c04aa7778b761f763c445ef0b
Nikiths1 <nikith.s1@mphasis.com>
2023-08-28 14:57:40 +0530

    first commit

---
+++ Controllers/BlogsController.cs
@@ -0,0 +1,44 @@
+﻿using BlogPlatform.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace BlogPlatform.Controllers
+{
+    public class BlogsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BlogsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var posts = await _context.Posts.ToListAsync();
+            return View(posts);
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var post = await _context.Posts
+                .Include(p => p.Comments)
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            return View(post);
+        }
+    }
+}
---
+++ Models/Comment.cs
@@ -0,0 +1,15 @@
+﻿using BlogPlatformMvc.Models;
+using System;
+
+namespace BlogPlatform.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public DateTime PublicationDate { get; set; }
+
+        public int PostId { get; set; }
+        public Post Post { get; set; }
+    }
+}
